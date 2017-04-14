1901f7399ba7278c43071a56ef12cb6353059a50
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-14 17:36:33 -0400

    Adds styling to modal, and signup button closes modal.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var modal = document.getElementsByClassName("modal-container")[0];
 var overlay = document.getElementsByClassName("overlay")[0];
 var closeSpan = document.getElementsByClassName("modal-close")[0];
+var signUpBtn = document.getElementsByClassName("signup-button")[0];
 
 setTimeout(function() {
 	modal.style.display = "block";
@@ -8,7 +9,7 @@ setTimeout(function() {
 }, 600);
 
 closeSpan.addEventListener("click", closeModal);
-
+signUpBtn.addEventListener("click", closeModal);
 
 document.addEventListener("keyup", function() {
 	if (event.keyCode === 27) {
