0bd144e7dc7ef190658fae9146368b4e8034df8e
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-14 15:07:45 -0400

    Adds popup functionality using setTimeout to change modal.style.display from 'none' to 'block'.

---
+++ script.js
@@ -0,0 +1,6 @@
+var modal = document.getElementsByClassName("modal-container")[0];
+var closeBtn = document.getElementsByClassName("modal-close")[0];
+
+setTimeout(function() {
+	modal.style.display = "block";
+}, 3000);
