243444ae2f4bcc2b957a278ff04405c586519e56
Sophia Di <backwardsxmarathon@gmail.com>
2017-04-14 15:30:43 -0400

    Close button closes modal.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var modal = document.getElementsByClassName("modal-container")[0];
-var closeBtn = document.getElementsByClassName("modal-close")[0];
+var closeSpan = document.getElementsByClassName("modal-close")[0];
 
 setTimeout(function() {
 	modal.style.display = "block";
-}, 3000);
+}, 800);
+
+closeSpan.addEventListener("click", closeModal);
+
+
+function closeModal() {
+	modal.style.display = "none";
+}
